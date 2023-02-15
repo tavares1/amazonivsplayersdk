0328a1c16a37f89aa2d751d9d34d184d888f840b
Lucas Tavares <lucastavaresvasconcelos@gmail.com>
2023-02-15 14:33:21 -0300

    update target name

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "amazonivsplayersdk",
+    name: "AmazonIVSPlayer",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "amazonivsplayersdk",
+            name: "AmazonIVSPlayer",
             targets: ["AmazonIVSPlayer"]),
     ],
     dependencies: [
