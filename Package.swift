fc872b0ddaf6f0d20900f3efae99ade220ee30e7
Lucas Tavares <lucastavaresvasconcelos@gmail.com>
2023-02-15 14:31:09 -0300

    binary target as main target

--- Package.swift
+++ Package.swift
@@ -9,26 +9,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "amazonivsplayersdk",
-            targets: ["AmazonIVSPlayerSDK"]),
+            targets: ["AmazonIVSPlayer"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-          name: "AmazonIVSPlayerSDK",
-          dependencies: [
-            "AmazonIVSPlayer"
-          ],
-          exclude: [
-            "*"
-          ], sources: [
-            "Frameworks"
-          ]
-        ),
         .binaryTarget(
             name: "AmazonIVSPlayer",
             path: "Frameworks/AmazonIVSPlayer.xcframework"
